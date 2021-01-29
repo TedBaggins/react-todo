61edffe876449dc177a5effa0706703d116eafc9
TedBaggins <tedbagginsrb@gmail.com>
2021-01-29 20:59:16 +0300

    todo-app

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+
+export const About = () => {
+    return (
+        <div className="jumbotron">
+            <div className="container">
+                <h1 className="display-4">Приложуха для дел там и всё такое...</h1>
+                <p className="lead">Версия приложения <strong>1.0</strong></p>
+            </div>
+        </div>
+    )
+}
